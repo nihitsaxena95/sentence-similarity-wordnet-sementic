fb3fbd664c0442e66d19b0aeb06e5f78614983c2
Nihit Saxena <nihitsaxena@Nihits-MacBook-Air.local>
2019-04-13 19:47:38 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Sentence Similarity Using Wordnet
+
+Calculating the semantic similarity between sentences is a long dealt problem in the area of natural language processing.
+
+The semantic analysis field has a crucial role to play in the research related to the text analytics. The semantic similarity differs as the domain of operation differs. In this paper, we present a methodology which deals with this issue by incorporating semantic similarity and corpus statistics. To calculate the semantic similarity between words and sentences, the proposed method follows an edge-based approach using a lexical database. The methodology can be applied in a variety of domains. The methodology has been tested on both benchmark standards and mean human similarity dataset. When tested on these two datasets, it gives highest correlation value for both word and sentence similarity outperforming other similar models. For word similarity, we obtained Pearson correlation coefficient of 0.8753 and for sentence similarity, the correlation obtained is 0.8794.
+
+
+## THE METHODOLOGY
+
+The methodology considers the text as a sequence of words and deals with all the words in sentences separately according to their semantic and syntactic structure. The information content of the word is related to the frequency of the meaning of the word in a lexical database or a corpus. The method to calculate the semantic similarity between two sentences is divided into following parts:
+• Word similarity
+• Sentence similarity
+• Word order similarity
+
+## LIBRARY USED
+
+1) NLTK
+2) WORDNET CORPUS
+3) NUMPY
+
+## TEST DATA AND ANALYSIS
+
+COMING SOON...
+
+## Reference
+
+`https://arxiv.org/pdf/1802.05667.pdf`
---
+++ SentenceSimilarity.py
@@ -0,0 +1,76 @@
+"""
+Created on Fri Apr 12 15:10:40 2019
+
+@author: nihitsaxena
+"""
+
+import nltk
+from pywsd.lesk import simple_lesk
+import numpy as np
+
+class SentenceSimilarity:
+    
+    def __init__(self):
+        self.word_order = False
+        
+    
+    def identifyWordsForComparison(self, sentence):
+        #Taking out Noun and Verb for comparison word based
+        tokens = nltk.word_tokenize(sentence)        
+        pos = nltk.pos_tag(tokens)
+        pos = [p for p in pos if p[1].startswith('N') or p[1].startswith('V')]     
+        return pos
+    
+    def wordSenseDisambiguation(self, sentence):
+        # removing the disambiguity by getting the context
+        pos = self.identifyWordsForComparison(sentence)
+        sense = []
+        for p in pos:
+            sense.append(simple_lesk(sentence, p[0], pos=p[1][0].lower()))
+        return set(sense)
+    
+    def getSimilarity(self, arr1, arr2, vector_len):
+        #cross multilping all domains 
+        vector = [0.0] * vector_len
+        count = 0
+        for i,a1 in enumerate(arr1):
+            all_similarityIndex=[]
+            for a2 in arr2:
+                similarity = a1.wup_similarity(a2)
+                if similarity != None:
+                    all_similarityIndex.append(similarity)
+                else:
+                    all_similarityIndex.append(0.0)
+            all_similarityIndex = sorted(all_similarityIndex, reverse = True)
+            vector[i]=all_similarityIndex[0]
+            if vector[i] >= 0.804:
+                count +=1
+        return vector, count        
+        
+        
+    def shortestPathDistance(self, sense1, sense2):
+        #getting the shortest path to get the similarity
+        if len(sense1) >= len(sense2):
+            grt_Sense = len(sense1)
+            v1, c1 = self.getSimilarity(sense1, sense2, grt_Sense)
+            v2, c2 = self.getSimilarity(sense2, sense1, grt_Sense)
+        if len(sense2) > len(sense1):
+            grt_Sense = len(sense2)
+            v1, c1 = self.getSimilarity(sense2, sense1, grt_Sense)
+            v2, c2 = self.getSimilarity(sense1, sense2, grt_Sense)
+        return np.array(v1),np.array(v2),c1,c2
+        
+    def main(self, sentence1, sentence2):
+        sense1 = self.wordSenseDisambiguation(sentence1)
+        sense2 = self.wordSenseDisambiguation(sentence2)        
+        v1,v2,c1,c2 = self.shortestPathDistance(sense1,sense2)
+        dot = np.dot(v1,v2)
+        print("dot", dot) # getting the dot product
+        tow = (c1+c2)/1.8
+        final_similarity = dot/tow
+        print("similarity",final_similarity)
+
+obj = SentenceSimilarity()
+a = 'A jewel is a precious stone used to decorate valuable things that you wear, such as rings or necklaces.'
+b = 'A gem is a jewel or stone that is used in jewellery.'
+obj.main(a,b)
